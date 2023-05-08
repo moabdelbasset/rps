94bc55f4c520f620b321492deb34e85d5fd9b5bf
moabdelbasset <>
2023-05-08 18:08:02 +0000

    add favicon and initial JS wait for DOM to load

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//Wait for the DOM to finish loading before running the game
+//Get the button elements and add event listeners to them
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Your code here will run after the DOM is fully loaded
+
+    console.log('DOM is fully loaded');
+});
