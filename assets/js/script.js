22f2b39e37128d39299e76bcd6f4b5898d49b774
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2023-05-18 22:31:51 +0000

    removing the comments in the script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,126 +2,115 @@
 //Get the button elements and add event listeners to them
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Your code here will run after the DOM is fully loaded
-    // Naming the Rock, Paper, Scissors button
-    console.log('DOM is fully loaded');
-    let myArray = ['Rock', 'Paper', 'Scissors'];
-    let gameBtns = document.getElementsByClassName('gameBtns');
-    for (let i = 0; i < gameBtns.length; i++ ) {
-      gameBtns[i].innerHTML = myArray[i];
-    }
+  // Your code here will run after the DOM is fully loaded
+  // Naming the Rock, Paper, Scissors button
+  console.log('DOM is fully loaded');
+  var myArray = ['Rock', 'Paper', 'Scissors'];
+  var gameBtns = document.getElementsByClassName('gameBtns');
+  for (var i = 0; i < gameBtns.length; i++ ) {
+    gameBtns[i].innerHTML = myArray[i];
+  }
 });
 
 /**
- * loadNewPage function will be triggered when a user click on New game button to start a new game
- */
+* loadNewPage function will be triggered when a user click on New game button to start a new game
+*/
 function loadNewPage() {
-  window.location.href = "game.html";
+window.location.href = "game.html";
 }
 
 /**
- * Rock function is triggered when user click on rock button that will change the image to rock image
- */
+* Rock function is triggered when user click on rock button that will change the image to rock image
+*/
 function rock() {
-    var image = document.getElementById("full-img1");
-    image.src = "assets/images/rock.JPG";
-    image.alt = "Rock";
-    playerChoice = "rock";
-    computerTurn(playerChoice);
+  var image = document.getElementById("full-img1");
+  image.src = "assets/images/rock.JPG";
+  image.alt = "Rock";
+  playerChoice = "rock";
+  computerTurn(playerChoice);
 }
 
 /**
- * Paper function is triggered when user click on rock button that will change the image to paper image
- */
+* Paper function is triggered when user click on rock button that will change the image to paper image
+*/
 function paper() {
-    var image = document.getElementById("full-img1");
-    image.src = "assets/images/paper.JPG";
-    image.alt = "Paper";
-    playerChoice = "paper";
-    computerTurn(playerChoice);
+  var image = document.getElementById("full-img1");
+  image.src = "assets/images/paper.JPG";
+  image.alt = "Paper";
+  playerChoice = "paper";
+  computerTurn(playerChoice);
 }
 
 /**
- * Scissors function is triggered when user click on rock button that will change the image to scissors image
- */
+* Scissors function is triggered when user click on rock button that will change the image to scissors image
+*/
 function scissors() {
-    var image = document.getElementById("full-img1");
-    image.src = "assets/images/scissors.JPG";
-    image.alt = "Scissors";
-    playerChoice = "scissors";
-    computerTurn(playerChoice);
+  var image = document.getElementById("full-img1");
+  image.src = "assets/images/scissors.JPG";
+  image.alt = "Scissors";
+  playerChoice = "scissors";
+  computerTurn(playerChoice);
 }
 
 /**
- * computerTurn functions executes after player done selection
- */
+* computerTurn functions executes after player done selection
+*/
 function computerTurn(playerChoice) {
-    let num1 = Math.floor(Math.random() * 2);
-    console.log(num1)
-    let myArray = ['rock', 'paper', 'scissors'];
-    let computerChoice = ''
-    var image = document.getElementById("full-img2");
-    if (myArray[num1] == 'rock') {
-        image.src = "assets/images/rock.JPG";
-        image.alt = "Rock";
-        computerChoice = 'rock';
-        gamePlay(computerChoice, playerChoice)
-      } else if (myArray[num1] == 'paper') {
-        image.src = "assets/images/paper.JPG";
-        image.alt = "Paper";
-        computerChoice = 'paper';
-        gamePlay(computerChoice, playerChoice)
-      } else {
-        image.src = "assets/images/scissors.JPG";
-        image.alt = "Scissors";
-        computerChoice = 'scissors';
-        gamePlay(computerChoice, playerChoice)
-      }
+  var num1 = Math.floor(Math.random() * 2);
+  var myArray = ['rock', 'paper', 'scissors'];
+  var computerChoice = '';
+  var image = document.getElementById("full-img2");
+  if (myArray[num1] == 'rock') {
+      image.src = "assets/images/rock.JPG";
+      image.alt = "Rock";
+      computerChoice = 'rock';
+      gamePlay(computerChoice, playerChoice);
+    } else if (myArray[num1] == 'paper') {
+      image.src = "assets/images/paper.JPG";
+      image.alt = "Paper";
+      computerChoice = 'paper';
+      gamePlay(computerChoice, playerChoice);
+    } else {
+      image.src = "assets/images/scissors.JPG";
+      image.alt = "Scissors";
+      computerChoice = 'scissors';
+      gamePlay(computerChoice, playerChoice);
+    }
 
 }
 
 /**
- * The main function that will decide which player wins and calculate the score
- */
+* The main function that will decide which player wins and calculate the score
+*/
 function gamePlay(computerChoice, playerChoice) {
-  let playerScore = parseInt(document.getElementById("player1").innerText);
-  console.log(playerScore);
-  let computerScore = parseInt(document.getElementById("computer").innerText);
-  //document.getElementById("score").innerText = ++oldScore;
-    if (playerChoice === computerChoice) {
-        //alert(`It's a tie!`);
-        document.getElementById("score-text").innerHTML = "It's a tie!";
-        //resetImage();
-        //return "It's a tie!";
-      } else if (
-        (playerChoice === 'rock' && computerChoice === 'scissors') ||
-        (playerChoice === 'paper' && computerChoice === 'rock') ||
-        (playerChoice === 'scissors' && computerChoice === 'paper')
-      ) {
-        //alert(`You win! ${playerChoice} beats ${computerChoice}.`);
-        document.getElementById("score-text").innerHTML = `You win! ${playerChoice} beats ${computerChoice}.`;
-        document.getElementById("player1").innerText = ++playerScore;
-        //resetImage();
-        //return "You win! " + playerChoice + " beats " + computerChoice + ".";
-      } else {
-        //alert (`You lose! ${computerChoice} beats ${playerChoice}.`);
-        document.getElementById("score-text").innerHTML = `You lose! ${computerChoice} beats ${playerChoice}.`;
-        document.getElementById("computer").innerText = ++computerScore;
-        //resetImage();
-        //return "You lose! " + computerChoice + " beats " + playerChoice + ".";
-      }
+var playerScore = parseInt(document.getElementById("player1").innerText);
+console.log(playerScore);
+var computerScore = parseInt(document.getElementById("computer").innerText);
+  if (playerChoice === computerChoice) {
+      document.getElementById("score-text").innerHTML = "It's a tie!";
+    } else if (
+      (playerChoice === 'rock' && computerChoice === 'scissors') ||
+      (playerChoice === 'paper' && computerChoice === 'rock') ||
+      (playerChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+      document.getElementById("score-text").innerHTML = `You win! ${playerChoice} beats ${computerChoice}.`;
+      document.getElementById("player1").innerText = ++playerScore;
+    } else {
+      document.getElementById("score-text").innerHTML = `You lose! ${computerChoice} beats ${playerChoice}.`;
+      document.getElementById("computer").innerText = ++computerScore;
+    }
 }
 /**
- * Reset the image function to reset the image back to the original image
- */
+* Reset the image function to reset the image back to the original image
+*/
 function resetImage() {
-  var image1 = document.getElementById("full-img1");
-  var image2 = document.getElementById("full-img2");
-  image1.src = "assets/images/rps.JPG";
-  image1.alt = "RPS";
-  image2.src = "assets/images/rps.JPG";
-  image1.alt = "RPS";
-  document.getElementById("computer").innerText = 0;
-  document.getElementById("player1").innerText = 0;
-  document.getElementById("score-text").innerHTML = "";
+var image1 = document.getElementById("full-img1");
+var image2 = document.getElementById("full-img2");
+image1.src = "assets/images/rps.JPG";
+image1.alt = "RPS";
+image2.src = "assets/images/rps.JPG";
+image1.alt = "RPS";
+document.getElementById("computer").innerText = 0;
+document.getElementById("player1").innerText = 0;
+document.getElementById("score-text").innerHTML = "";
 }
